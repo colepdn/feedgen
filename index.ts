e590b2ce18b05aef12157c091b798536db1295ea
Cole Peden <colepeden@gmail.com>
2025-02-03 21:54:51 -0500

    add hard-coded .well-known

--- index.ts
+++ index.ts
@@ -5,12 +5,17 @@ const port = 3000
 
 https://feedgen.hotgoth.mom/xrpc/app.bsky.feed.getFeedSkeleton?feed=at://did:plc:wl4wyug27klcee5peb3xkeut/app.bsky.feed.generator/boner
 
+app.get('/.well-known/did.json', async (_req, res) => {
+	res.status(200)
+	res.json({"@context":["https://www.w3.org/ns/did/v1"],"id":"did:web:feedgen.hotgoth.mom","service":[{"id":"#bsky_fg","type":"BskyFeedGenerator","serviceEndpoint":"https://feedgen.hotgoth.mom"}]})
+})
+
 app.get('/xrpc/app.bsky.feed.getFeedSkeleton', async (req, res) => {
 	console.log('req', req)
 	console.log('query', req.query)
 	const token = req.headers.authorization
 	console.log('token', token)
-	if (false && token && token.includes("Bearer")) {
+	if (token && token.includes("Bearer")) {
 		console.log('decoded', jwt(req.headers.authorization)[1])
 		res.status(200)
 		// auth'd  
