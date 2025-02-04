71d10a2844272ac78ff377b0e55ff033ccf31e5b
Cole Peden <cole.peden@gmail.com>
2025-02-04 12:13:12 -0500

    initial cron

--- cron.ts
+++ cron.ts
@@ -1,2 +1,19 @@
+import { XRPC, CredentialManager } from '@atcute/client'
 import Database from 'better-sqlite3'
 const db = new Database()
+import 'dotenv/config'
+
+const manager = new CredentialManager({ service: process.env.BSKY_SERVICE })
+const rpc = new XRPC({ handler: manager })
+
+await manager.login({ identifier: process.env.BSKY_HANDLE, password: process.env.BSKY_PASSWORD })
+
+console.log(manager.session)
+
+const { data } = await rpc.get('app.bsky.feed.getLikes', {
+	params: {
+		uri: ""
+	},
+});
+
+console.log(data)
