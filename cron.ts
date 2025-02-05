2e1e1511ad4c8b613dcd16caa9ae801c2bcf844d
Cole Peden <colepeden@gmail.com>
2025-02-04 19:49:31 -0500

    stuff!

--- cron.ts
+++ cron.ts
@@ -12,8 +12,56 @@ console.log(manager.session)
 
 const { data } = await rpc.get('app.bsky.feed.getLikes', {
 	params: {
-		uri: ""
+		uri: "at://did:plc:wl4wyug27klcee5peb3xkeut/app.bsky.feed.generator/feed"
 	},
 });
 
-console.log(data)
+for ( const item of data.likes ) {
+	console.log(`${item.actor.handle}: ${item.actor.did}`)
+	let follows = []
+	let cursor = null
+	while (cursor !== undefined) {
+		const { data: d } = await rpc.get("app.bsky.graph.getFollows", {
+			params: {
+				actor: item.actor.did,
+				//actor: "did:plc:mt74bxrck624eg4atq2cpgtg",
+				cursor: cursor ?? undefined
+			}
+		})
+		console.log(d.follows.length, d.cursor)
+		follows = follows.concat(d.follows)
+		cursor = d.cursor
+	}
+	console.log('done! len:', follows.length)
+
+	for (const follow of follows.slice(0, 10)) {
+		console.log(follow.did)
+		const { data: authorFeed } = await rpc.get('app.bsky.feed.getAuthorFeed', {
+			params: {
+				actor: follow.did,
+				filter: "posts_no_replies"
+			}
+		})
+		console.log(authorFeed)
+		for (const post of authorFeed.feed) {
+			//console.log('p', post.post.record.text, 'r', post.reason)
+			if (post.reason?.['$type'] === 'app.bsky.feed.defs#reasonRepost') {
+				console.log('skipping... repost')
+			}
+			console.log(post.post)
+		}
+		break;
+	}
+	
+}
+
+
+/*
+const { data: myFeed } = await rpc.get('app.bsky.feed.getAuthorFeed', {
+	params: {
+		actor: "did:plc:wl4wyug27klcee5peb3xkeut"
+	}
+})
+
+console.log(myFeed) 
+*/
