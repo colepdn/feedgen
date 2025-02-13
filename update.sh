87ed0caaf4d91d73813ec3ba0eb2a66706451fbc
cole peden <cole.peden@outlook.com>
2025-02-12 23:29:41 -0500

    update plan

--- update.sh
+++ update.sh
@@ -5,3 +5,4 @@ sudo cp /var/lib/feedgen/feedgen-cron.timer /etc/systemd/system
 sudo cp /var/lib/feedgen/feedgend.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo chown -R feedgen:feedgen /var/lib/feedgen
+sudo systemctl restart feedgend
